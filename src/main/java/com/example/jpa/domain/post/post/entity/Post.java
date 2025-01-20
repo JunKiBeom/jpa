caf38151076fc132cbbeecad8e817000a976d0e5
JunKiBeom <dona0408@gmail.com>
2025-01-20 10:42:46 +0900

    BaseInitData 작성

--- src/main/java/com/example/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/example/jpa/domain/post/post/entity/Post.java
@@ -1,16 +1,20 @@
 package com.example.jpa.domain.post.post.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Setter
+@Getter
 public class Post {
 
     @Id // Primary Key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto Increment
     private long id;
-    private LocalDateTime localDate;
+    private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
     @Column(length = 100)
     private String title;
