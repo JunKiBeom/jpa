94f83d583cc67aa89681be179f5d2c826017b889
JunKiBeom <dona0408@gmail.com>
2025-01-20 15:14:08 +0900

    insert 확인 & findBy

--- src/main/java/com/example/jpa/global/BaseInitData.java
+++ src/main/java/com/example/jpa/global/BaseInitData.java
@@ -12,23 +12,31 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @RequiredArgsConstructor
-public class BaseInitData
-{
+public class BaseInitData {
 
     private final PostService postService;
 
     @Bean
     @Order(1)
     public ApplicationRunner applicationRunner() {
-        return  args -> {
-
+        return args -> {
+            // 샘플 데이터 3개 생성.
+            // 데이터가 3개가 이미 있으면 패스
             if( postService.count() > 0 ) {
                 return ;
             }
 
+            System.out.println("==== 1번 데이터 생성 ====");
             postService.write("title1", "body1");
+            System.out.println("==== 1번 데이터 생성 완료 ====");
+
+            System.out.println("==== 2번 데이터 생성 ====");
             postService.write("title2", "body2");
+            System.out.println("==== 2번 데이터 생성 완료 ====");
+
+            System.out.println("==== 3번 데이터 생성 완료 ====");
             postService.write("title3", "body3");
+            System.out.println("==== 3번 데이터 생성 완료 ====");
 
         };
     }
@@ -36,15 +44,6 @@ public class BaseInitData
     @Bean
     @Order(2)
     public ApplicationRunner applicationRunner2() {
-//        return args -> {
-//            Post post = postService.findById(1L).get();
-//
-//            Thread.sleep(1000);
-//
-//            postService.modify(post, "new title", "new body");
-////            postService.modify2(1L, "new title2", "new body2");
-//        };
-
         return new ApplicationRunner() {
             @Override
             @Transactional
@@ -71,15 +70,32 @@ public class BaseInitData
                 postService.delete(p1);
                 System.out.println("====== p1 삭제 완료 ======");
 
-
                 System.out.println("====== p2 삭제 ======");
                 postService.delete(p2);
                 System.out.println("====== p2 삭제 완료 ======");
 
-//                postService.deleteById(1L);
-//                postService.deleteById(2L);
+            }
+        };
+    }
+
+    @Bean
+    @Order(4)
+    public ApplicationRunner applicationRunner4() {
+        return new ApplicationRunner() {
+            @Override
+            @Transactional
+            public void run(ApplicationArguments args) throws Exception {
+
+                Post post = postService.findById(3L).get();
+                System.out.println(post.getId() + "번 포스트를 가져왔습니다.");
+                System.out.println("==========================================");
+
+                Post post2 = postService.findById(3L).get();
+                System.out.println(post2.getId() + "번 포스트를 가져왔습니다.");
+
 
             }
         };
     }
+
 }
