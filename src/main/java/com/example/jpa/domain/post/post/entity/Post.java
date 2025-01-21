5be25dc387501b2b3498d5000e2226b509b3e0fe
JunKiBeom <dona0408@gmail.com>
2025-01-21 14:33:43 +0900

    OneToMany, ManyToOne

--- src/main/java/com/example/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/example/jpa/domain/post/post/entity/Post.java
@@ -1,5 +1,6 @@
 package com.example.jpa.domain.post.post.entity;
 
+import com.example.jpa.domain.post.comment.entity.Comment;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -7,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -34,11 +36,7 @@ public class Post {
     @Column(columnDefinition = "TEXT")
     private String body;
 
-//    @OneToMany(mappedBy="post", cascade = CascadeType.ALL)
-//    @Builder.Default
-//    private List<Comment> comments = new ArrayList<>();
+    @OneToMany(mappedBy = "post") // mappedBy를 사용하지 않은 쪽이 주인
+    private List<Comment> comment;
 
-//    public void addComment(Comment comment) {
-//        comments.add(comment);
-//    }
 }
